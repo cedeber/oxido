3a0e6d427c2dd2d58b9bea3d4e050e25ddf85813
Cédric Eberhardt <hello+code@cedeber.fr>
2020-02-05 17:43:07 +0100

    Add tests

--- simple_web/src/lib.rs
+++ simple_web/src/lib.rs
@@ -18,3 +18,13 @@ pub fn main() -> Result<(), JsValue> {
 pub fn add(a: i32, b: i32) -> i32 {
     a + b
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(5, add(2, 3));
+    }
+}
