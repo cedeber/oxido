e0619341c0a8efe5be95d80a7b5aa1350311cecf
Cédric Eberhardt <hello+code@cedeber.fr>
2020-03-23 20:29:35 +0100

    Add MouseEvent

--- game-of-life/src/lib.rs
+++ game-of-life/src/lib.rs
@@ -1,8 +1,8 @@
-extern crate wasm_bindgen;
-
 use std::f64;
+use std::rc::Rc;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
+use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement, MouseEvent};
 
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
@@ -18,28 +18,46 @@ pub fn main() -> Result<(), JsValue> {
 
 #[wasm_bindgen]
 pub fn setup(width: u32, height: u32) -> Result<(), JsValue> {
-    let document = web_sys::window().unwrap().document().unwrap();
-    // let canvas = document.get_element_by_id("canvas").unwrap();
-    // let canvas: web_sys::HtmlCanvasElement = canvas
-    //     .dyn_into::<web_sys::HtmlCanvasElement>()
-    //     .map_err(|_| ())
-    //     .unwrap();
+    let window = web_sys::window().expect("should have a window in this context");
+    let document = window.document().expect("window should have a document");
     let canvas = document
         .create_element("canvas")?
-        .dyn_into::<web_sys::HtmlCanvasElement>()?;
+        .dyn_into::<HtmlCanvasElement>()?;
     document.body().unwrap().append_child(&canvas)?;
     canvas.set_width(width);
     canvas.set_height(height);
     canvas.style().set_property("border", "solid")?;
-    canvas.style().set_property("image-rendering", "crisp-edges")?;
+    canvas
+        .style()
+        .set_property("image-rendering", "crisp-edges")?;
 
     let context = canvas
         .get_context("2d")
         .unwrap()
         .unwrap()
-        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .dyn_into::<CanvasRenderingContext2d>()
         .unwrap();
 
+    let context = Rc::new(context);
+
+    {
+        // MouseEvent Closure
+        let context = context.clone();
+        let closure = Closure::wrap(Box::new(move |event: MouseEvent| {
+            draw(&context, Some(event), &width, &height)
+        }) as Box<dyn FnMut(_)>);
+
+        canvas.add_event_listener_with_callback("mousemove", closure.as_ref().unchecked_ref())?;
+        closure.forget();
+    }
+
+    draw(&context, None, &width, &height);
+
+    Ok(())
+}
+
+fn draw(context: &CanvasRenderingContext2d, event: Option<MouseEvent>, width: &u32, height: &u32) {
+    context.clear_rect(0.0, 0.0, *width as f64, *height as f64);
     context.begin_path();
 
     // Draw the outer circle.
@@ -63,7 +81,16 @@ pub fn setup(width: u32, height: u32) -> Result<(), JsValue> {
         .arc(90.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
         .unwrap();
 
-    context.stroke();
+    // Draw the mouse lines
+    match event {
+        Some(e) => {
+            context.move_to(e.offset_x() as f64, 0.0);
+            context.line_to(e.offset_x() as f64, *height as f64);
+            context.move_to(0.0, e.offset_y() as f64);
+            context.line_to(*width as f64, e.offset_y() as f64);
+        }
+        None => {}
+    }
 
-    Ok(())
+    context.stroke();
 }
