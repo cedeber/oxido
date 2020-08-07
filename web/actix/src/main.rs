6ca5e25f4045f4cbf57fb7ec8874474f4ba71b24
Cédric Eberhardt <hello+code@cedeber.fr>
2020-08-08 01:52:10 +0200

    Update actix

--- web/actix/src/main.rs
+++ web/actix/src/main.rs
@@ -1,22 +1,21 @@
 use actix_web::{get, middleware, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
 
 #[get("/hello")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+#[derive(Deserialize)]
 struct Info {
     name: String,
 }
 
-async fn _index(path: web::Path<Info>) -> String {
-    format!("Welcome {}!", path.name)
-}
-
-async fn index(req: HttpRequest) -> String {
+async fn index(req: HttpRequest, path: web::Path<Info>) -> String {
     println!("REQ: {:?}", req);
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+    // let name = req.match_info().get("name").unwrap_or("World");
+    // format!("Hello {}!", &name)
+    format!("Hello {}!", path.name) // If "/" => missing field `name`
 }
 
 #[actix_rt::main]
