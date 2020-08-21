9b05aadc84f8393ed54fdc2e1327c2e3b454c4ab
Cédric Eberhardt <hello+code@cedeber.fr>
2020-08-22 01:35:35 +0200

    we have sprites!

--- amethyst_sprite_animation/src/main.rs
+++ amethyst_sprite_animation/src/main.rs
@@ -1,4 +1,7 @@
+mod pong;
+
 use amethyst::{
+    core::TransformBundle,
     prelude::*,
     renderer::{
         plugins::{RenderFlat2D, RenderToWindow},
@@ -8,27 +11,30 @@ use amethyst::{
     utils::application_root_dir,
 };
 
-pub struct Pong;
-
-impl SimpleState for Pong {}
+use crate::pong::Pong;
 
 fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
 
     let app_root = application_root_dir()?;
     let display_config_path = app_root.join("config").join("display.ron");
-    let game_data = GameDataBuilder::default().with_bundle(
-        RenderingBundle::<DefaultBackend>::new()
-            // The RenderToWindow plugin provides all the scaffolding for opening a window and drawing on it
-            .with_plugin(
-                RenderToWindow::from_config_path(display_config_path)?
-                    .with_clear([0.0, 0.0, 0.0, 1.0]),
-            )
-            // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
-            .with_plugin(RenderFlat2D::default()),
-    )?;
 
     let assets_dir = app_root.join("assets");
+
+    let game_data = GameDataBuilder::default()
+        // Add the transform bundle which handles tracking entity positions
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(
+            RenderingBundle::<DefaultBackend>::new()
+                // The RenderToWindow plugin provides all the scaffolding for opening a window and drawing on it
+                .with_plugin(
+                    RenderToWindow::from_config_path(display_config_path)?
+                        .with_clear([0.0, 0.0, 0.0, 1.0]),
+                )
+                // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
+                .with_plugin(RenderFlat2D::default()),
+        )?;
+
     let mut game = Application::new(assets_dir, Pong, game_data)?;
 
     game.run();
