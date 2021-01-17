72b93ba516ac4003eb193c85a86e2f0eba281035
Cédric Eberhardt <hello+code@cedeber.fr>
2021-01-17 11:39:47 +0100

    little improvements

--- simple_web/worker.js
+++ simple_web/worker.js
@@ -1,8 +1,6 @@
 import init, { add } from "../pkg/simple_web.js";
 
-// Path is mandatory for worker because rollup replace "import.meta.url"
-// with document-related APIs, which is not available in worker.
-let ready = init("../pkg/simple_web_bg.wasm");
+let ready = init();
 let ctx = self;
 
 ctx.addEventListener("message", event => {
