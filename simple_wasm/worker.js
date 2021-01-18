19bd33198bda1f464ccfdc93e28b697379363da4
Cédric Eberhardt <hello+code@cedeber.fr>
2021-01-18 09:21:02 +0100

    use old way of loading wasm because webkit

--- simple_wasm/worker.js
+++ simple_wasm/worker.js
@@ -3,9 +3,30 @@ let ctx = self;
 ctx.addEventListener("message", event => {
     let { a, b } = event.data;
 
+    /*
+    // Not compatible with Webkit
     WebAssembly.instantiateStreaming(fetch('./target/wasm32-wasi/release/simple_wasm.wasm'))
         .then(obj => {
             let { add } = obj.instance.exports;
             ctx.postMessage(add(a, b));
         });
+
+    WebAssembly.compileStreaming(fetch('./target/wasm32-wasi/release/simple_wasm.wasm'))
+        .then(module => WebAssembly.instantiate(module))
+        .then(instance => {
+            let { add } = instance.exports;
+            ctx.postMessage(add(a, b));
+        });
+    */
+
+    let importObject = {};
+
+    fetch('./target/wasm32-wasi/release/simple_wasm.wasm').then(response =>
+        response.arrayBuffer()
+    ).then(bytes =>
+        WebAssembly.instantiate(bytes, importObject)
+    ).then(obj => {
+        let { add } = obj.instance.exports;
+        ctx.postMessage(add(a, b));
+    });
 });
