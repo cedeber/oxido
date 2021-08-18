9463137a619701e462f141d9d54eb868f4ad43df
Cédric Eberhardt <hello+code@cedeber.fr>
2021-08-18 20:38:22 +0200

    More details for wasm

--- simple_web/src/lib.rs
+++ simple_web/src/lib.rs
@@ -7,12 +7,18 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
     #[cfg(debug_assertions)]
+    #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
 
     Ok(())
 }
 
-#[wasm_bindgen()]
+#[wasm_bindgen]
+extern "C" {
+    // From Javascript
+}
+
+#[wasm_bindgen]
 pub fn add(a: i32, b: i32) -> i32 {
     a + b
 }
