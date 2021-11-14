c9a15d48258d238f7f62c3efaf110df647302172
Cédric Eberhardt <hello@cedeber.fr>
2021-11-14 13:41:22 +0100

    Rust 2021 + upgrade clap

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,27 +1,27 @@
-use clap::clap_app;
+use clap::{App, Arg};
 use console::style;
 use console::Term;
 use minigrep::Config;
 use std::{env, error::Error};
 
-// Use `wee_alloc` as the global allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 fn main() -> Result<(), Box<dyn Error>> {
     let term = Term::stdout();
 
-    let matches = clap_app!(gpxtools =>
-        (name: "Minigrep")
-        (version: "0.1.0-beta.1")
-        (author: "Cédric Eberhardt <hello+code@cedeber.fr>")
-        (about: "The toolbox for searching in files")
-        (@arg QUERY: +required "Sets the query string to search")
-        (@arg INPUT: +required "Sets the input file to use")
-        // (@arg verbose: -v --verbose "Print information verbosely")
-    )
-    .get_matches();
+    let matches = App::new("Minigrep")
+        .version("0.1.0-beta.1")
+        .author("Cédric Eberhardt <hello+code@cedeber.fr>")
+        .about("The toolbox for searching in files")
+        .arg(
+            Arg::new("QUERY")
+                .required(true)
+                .about("Sets the query string to search"),
+        )
+        .arg(
+            Arg::new("INPUT")
+                .required(true)
+                .about("Sets the input file to use"),
+        )
+        .get_matches();
 
     let query = matches.value_of("QUERY").unwrap();
     let filename = matches.value_of("INPUT").unwrap();
