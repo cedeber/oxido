b941abe0c6b4f776f18835963c8e4e629060b822
Cédric Eberhardt <hello@cedeber.fr>
2022-01-13 20:50:34 +0100

    Add JSON

--- axum/src/main.rs
+++ axum/src/main.rs
@@ -1,9 +1,10 @@
 use axum::{
     http::StatusCode,
     response::{Html, IntoResponse},
-    routing::get,
-    Router, Server,
+    routing::{get, post},
+    Json, Router, Server,
 };
+use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
 use tracing::debug;
 
@@ -13,7 +14,9 @@ async fn main() {
     tracing_subscriber::fmt::init();
 
     // build our application with a route
-    let app = Router::new().route("/", get(hello));
+    let app = Router::new()
+        .route("/", get(hello))
+        .route("/users", post(create_user));
 
     // run our app with hyper
     // `axum::Server` is a re-export of `hyper::Server`
@@ -27,4 +30,33 @@ async fn main() {
 
 async fn hello() -> impl IntoResponse {
     (StatusCode::NOT_FOUND, Html("Hello, World!"))
+}
+
+async fn create_user(
+    // this argument tells axum to parse the request body
+    // as JSON into a `CreateUser` type
+    Json(payload): Json<CreateUser>,
+) -> impl IntoResponse {
+    // insert your application logic here
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+
+    // this will be converted into a JSON response
+    // with a status code of `201 Created`
+    (StatusCode::CREATED, Json(user))
+}
+
+// the input to our `create_user` handler
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+// the output to our `create_user` handler
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
 }
